6424f5e31729e90afbf05c5f039ac4a867594c6c
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-03-30 21:06:57 +0800

    Update person.js

--- person.js
+++ person.js
@@ -16,45 +16,45 @@ class Person {
 		
 		if(this.character ==  "a"){
 			console.log("grrr");
-			const texture = PIXI.Texture.from('img/character_r.png');
+			const texture = PIXI.Texture.from('img/character1a.png');
 			this.sprite = new PIXI.Sprite(texture);
 			this.sprite.x = this.x;
 	    	this.sprite.y = this.y;
 	    	this.sprite.width = this.w;
 	    	this.sprite.height = this.h;
-	    	this.type = '0112';
+	    	this.type = '012';
 		}
 		if(this.character ==  "b"){
-			const texture = PIXI.Texture.from('img/character_n3.png');
+			const texture = PIXI.Texture.from('img/character1b.png');
 			console.log("geeeeee");
 			this.sprite = new PIXI.Sprite(texture);
 			this.sprite.x = this.x;
 	    	this.sprite.y = this.y;
-	    	this.sprite.width = this.w;
+	        this.sprite.width = this.w;
 	    	this.sprite.height = this.h;
 	    	this.type = '012';
 		}
 
 		if(this.character ==  "c"){
-			const texture = PIXI.Texture.from('img/character_n3.png');
+			const texture = PIXI.Texture.from('img/character1c.png');
 			console.log("geeeeee");
 			this.sprite = new PIXI.Sprite(texture);
 			this.sprite.x = this.x;
 	    	this.sprite.y = this.y;
-	    	this.sprite.width = this.w;
+	        this.sprite.width = this.w;
 	    	this.sprite.height = this.h;
-	    	this.type = '012';
+	    	this.type = '0445';
 		}
 
 		if(this.character ==  "d"){
-			const texture = PIXI.Texture.from('img/character_n3.png');
+			const texture = PIXI.Texture.from('img/character1d.png');
 			console.log("geeeeee");
 			this.sprite = new PIXI.Sprite(texture);
 			this.sprite.x = this.x;
 	    	this.sprite.y = this.y;
-	    	this.sprite.width = this.w;
+	        this.sprite.width = this.w;
 	    	this.sprite.height = this.h;
-	    	this.type = '012';
+	    	this.type = '0112';
 		}
 
 		this.sprite.interactive = true;
