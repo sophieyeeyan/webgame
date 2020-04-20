fb94e2f0daf481683ded584172a301f5b8f28605
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-04-20 19:01:13 +0800

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,10 @@ var abtnextTexture = PIXI.Texture.from('img/abtnext1.png');
 var abtnextTextureDown = PIXI.Texture.from('img/abtnext3.png');
 var abtnextTextureOver = PIXI.Texture.from('img/abtnext2.png');
 
+var starttexture = PIXI.Texture.fromVideo('vid/cover.mov');
+
+
+
 const styleXXX = new PIXI.TextStyle({
     fontSize: 40,
     fontFamily: 'Courier',
@@ -18,8 +22,26 @@ class StartScene extends Scene {
 
     constructor() {
     	super();
-    }
+        this.cover = new PIXI.Sprite(starttexture);
+        this.cover.width = 1300;
+        this.cover.height = 800;
+        this.addChild(this.cover);
 
+animate();
+
+//function animate(){
+
+    // render the stage
+    //renderer.render(stage);
+
+    //requestAnimationFrame(animate);
 }
+
+  }
+
+
+
+
+
 
     	
