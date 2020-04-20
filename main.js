5a8ed64ebde27de7df108694b589afe03b807551
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-04-20 19:01:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var scenesManager = new ScenesManager();
 scenesManager.create(1300, 800);
 //antialias: true
 
-var start = scenesManager.createScene('start', StartScene);
+//var start = scenesManager.createScene('start', StartScene);
 var game = scenesManager.createScene('game', GameScene);
 
 scenesManager.goToScene('game');
