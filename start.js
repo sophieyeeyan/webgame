90286993af35e92eb364017d344d077d6e4d7129
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-04-20 18:01:04 +0800

    Update start.js

--- start.js
+++ start.js
@@ -2,13 +2,13 @@ var abtnextTexture = PIXI.Texture.from('img/abtnext1.png');
 var abtnextTextureDown = PIXI.Texture.from('img/abtnext3.png');
 var abtnextTextureOver = PIXI.Texture.from('img/abtnext2.png');
 
-const style40 = new PIXI.TextStyle({
+const styleXXX = new PIXI.TextStyle({
     fontSize: 40,
     fontFamily: 'Courier',
     fill: 0x4A4879
 });
 
-const style20 = new PIXI.TextStyle({
+const styleXXXX = new PIXI.TextStyle({
     fontSize: 20,
     fontFamily: 'Courier',
     fill: 0x4A4879
@@ -17,5 +17,9 @@ const style20 = new PIXI.TextStyle({
 class StartScene extends Scene {
 
     constructor() {
+    	super();
+    }
+
+}
 
     	
