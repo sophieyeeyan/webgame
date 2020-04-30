ad335d593d59c73bfca70a9327f7319ea3fa910f
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-04-30 18:06:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ scenesManager.create(1300, 800);
 
 var start = scenesManager.createScene('start', StartScene);
 var game = scenesManager.createScene('game', GameScene);
-//var game2 = scenesManager.createScene('game2', Game2Scene);
+var two = scenesManager.createScene('two', TwoScene);
 //var game3 = scenesManager.createScene('game3', Game3Scene);
 
 
