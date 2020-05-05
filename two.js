e3b7d77d7e9c5e5afa34a26201a2b390d24fda06
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-05-05 16:16:06 +0800

    Update two.js

--- two.js
+++ two.js
@@ -1,29 +1,6 @@
-//const b_gap = 10;
-const buttons = [
 
-    {img:'img/button1.png', w:134, h:72, x:b_gap, y:0, speechImg:'img/speech21.png'},
-    {img:'img/button2.png', w:72, h:72, x:134+b_gap, y:0, speechImg:'img/speech2.png'},
-    {img:'img/button3.png', w:72, h:72, x:134+72+b_gap, y:0, speechImg:'img/speech3.png'},
-    {img:'img/button4.png', w:72, h:72, x:134+72+72+b_gap, y:0, speechImg:'img/speech4.png'},
-    {img:'img/button5.png', w:72, h:72, x:b_gap, y:60+b_gap, speechImg:'img/speech5.png'},
-    {img:'img/button6.png', w:72, h:72, x:72+b_gap, y:60+b_gap, speechImg:'img/speech6.png'},
-    {img:'img/button7.png', w:72, h:72, x:(72*2)+b_gap, y:60+b_gap, speechImg:'img/speech7.png'},
-    {img:'img/button8.png', w:72, h:72, x:(72*3)+b_gap, y:60+b_gap, speechImg:'img/speech8.png'},
-    {img:'img/button9.png', w:72, h:72, x:(72*4)+b_gap, y:60+b_gap, speechImg:'img/speech9.png'},
-    {img:'img/button10.png', w:72, h:72, x:b_gap, y:(65*2)+b_gap, speechImg:'img/speech10.png'},
-    {img:'img/button11.png', w:72, h:72, x:72+b_gap, y:(65*2)+b_gap, speechImg:'img/speech11.png'},
-    {img:'img/button12.png', w:72, h:72, x:(72*2)+b_gap, y:(65*2)+b_gap, speechImg:'img/speech12.png'},
-    {img:'img/button13.png', w:72, h:72, x:(72*3)+b_gap, y:(65*2)+b_gap, speechImg:'img/speech13.png'},
-    {img:'img/button14.png', w:72, h:72, x:(72*4)+b_gap, y:(65*2)+b_gap, speechImg:'img/speech14.png'},
-    {img:'img/button15.png', w:72, h:72, x:(72*4)+b_gap, y:(70*3)+b_gap, speechImg:'img/speech15.png'},
-    {img:'img/button16.png', w:72, h:72, x:b_gap, y:(70*3)+b_gap, speechImg:'img/speech16.png'},
-    {img:'img/button17.png', w:72, h:72, x:72+b_gap, y:(70*3)+b_gap, speechImg:'img/speech17.png'},
-    {img:'img/button18.png', w:72, h:72, x:(72*2)+b_gap, y:(70*3)+b_gap, speechImg:'img/speech18.png'},
-    {img:'img/button19.png', w:72, h:72, x:(72*3)+b_gap, y:(70*3)+b_gap, speechImg:'img/speech19.png'},
-    {img:'img/button20.png', w:72, h:72, x:(72*4)+b_gap, y:(70*3)+b_gap, speechImg:'img/speech20.png'},
-];
 
-const codes = [
+var codes = [
     "012",
     "0334",
     "0112",
@@ -57,20 +34,8 @@ var abtnextTexture = PIXI.Texture.from('img/abtnext1.png');
 var abtnextTextureDown = PIXI.Texture.from('img/abtnext3.png');
 var abtnextTextureOver = PIXI.Texture.from('img/abtnext2.png');
 
-let imageMode = PIXI.SCALE_MODES.LINEAR;
+//var imageMode = PIXI.SCALE_MODES.LINEAR;
 
-
-const style40 = new PIXI.TextStyle({
-    fontSize: 40,
-    fontFamily: 'Courier',
-    fill: 0x4A4879
-});
-
-const style20 = new PIXI.TextStyle({
-    fontSize: 20,
-    fontFamily: 'Courier',
-    fill: 0x4A4879
-});
 
 class TwoScene extends Scene {
 
