c44ea00831d4dec1cbabe7aa160a270b6b4b2b32
sophieyeeyan <61698278+sophieyeeyan@users.noreply.github.com>
2020-05-18 21:01:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ scenesManager.create(1300, 800);
 //antialias: true
 
 var start = scenesManager.createScene('start', StartScene);
-var game = scenesManager.createScene('game', GameScene);
-var two = scenesManager.createScene('two', TwoScene);
+var cut = scenesManager.createScene('cut', CutScene);
+
+var gameLevels = []
+var GameScene = [GameScene1, GameScene2, GameScene3, GameScene4, GameScene5]
+for (var i = 0; i < GameScene.length; i++) {
+	gameLevels[i] = scenesManager.createScene('game' + (i+1), GameScene[i]);
+}
+
+//gameLevels[1] = scenesManager.createScene('game' + 2, TwoScene);
+
+console.log('game' + 1)
+console.log('game' + 2)
+
 //var game3 = scenesManager.createScene('game3', Game3Scene);
 
 
